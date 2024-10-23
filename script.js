29d93ae7e47d00923cd25f78b17d669663ffb5e3
Bgabri <76167293+Bgabri@users.noreply.github.com>
2024-10-23 18:31:26 +0200

    css changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 !function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (!d.getElementById(id)) {
@@ -11,35 +12,27 @@
 
 fetch('https://services.swpc.noaa.gov/text/3-day-forecast.txt')
     .then(response => {
-        // Check if the response is OK
         if (!response.ok) {
             throw new Error('Network response was not ok ' + response.statusText);
         }
-        // Return the response text
         return response.text();
     })
     .then(data => {
-        // Set the text inside the element with id="aurora-forecast"
         document.getElementById('aurora-forecast').textContent = data;
     })
     .catch(error => {
-        // Display error message in case of failure
         document.getElementById('aurora-forecast').textContent = 'Error fetching the forecast: ' + error.message;
     });
 
 
-
-// Helper function to calculate the squared distance between two colors
 function colorDistance(c1, c2) {
     return (c1.r - c2.r) ** 2 + (c1.g - c2.g) ** 2 + (c1.b - c2.b) ** 2;
 }
 
-// Function to find the closest color from the palette
 function findClosestPaletteColor(r, g, b, palette) {
     let closestColor = palette[0];
     let minDistance = colorDistance({ r, g, b }, closestColor);
 
-    // Loop through the palette and find the color with the smallest distance
     for (let i = 1; i < palette.length; i++) {
         const distance = colorDistance({ r, g, b }, palette[i]);
         if (distance < minDistance) {
@@ -51,9 +44,46 @@ function findClosestPaletteColor(r, g, b, palette) {
     return closestColor;
 }
 
-// Wait until the window is fully loaded
-window.onload = function () {
+function distributeError(imageData, x, y, errR, errG, errB) {
+    function applyError(x, y, factor) {
+        if (x >= 0 && x < imageData.width && y >= 0 && y < imageData.height) {
+            let index = (y * imageData.width + x) * 4;
+            imageData.data[index] = imageData.data[index] + errR * factor;
+            imageData.data[index + 1] = imageData.data[index + 1] + errG * factor;
+            imageData.data[index + 2] = imageData.data[index + 2] + errB * factor;
+        }
+    }
+    // Floyd-Steinberg error distribution (right and below)
+    applyError(x + 1, y, 7 / 16);
+    applyError(x - 1, y + 1, 3 / 16);
+    applyError(x, y + 1, 5 / 16);
+    applyError(x + 1, y + 1, 1 / 16);
+}
 
+function quantize(imageData, palette) {
+    for (let y = 0; y < imageData.height; y++) {
+        for (let x = 0; x < imageData.width; x++) {
+            let index = (y * imageData.width + x) * 4;
+            let r = imageData.data[index];
+            let g = imageData.data[index + 1];
+            let b = imageData.data[index + 2];
+
+            let c = findClosestPaletteColor(r, g, b, palette);
+
+            imageData.data[index] = c.r;
+            imageData.data[index + 1] = c.g;
+            imageData.data[index + 2] = c.b;
+
+            // distributeError(imageData, x, y, r-c.r, g-c.g, b-c.b)
+
+        }
+
+    }
+
+    return imageData;
+}
+
+window.onload = function () {
     const palette = [
         { r: 29, g: 32, b: 33 },
         { r: 40, g: 40, b: 40 },
@@ -83,10 +113,7 @@ window.onload = function () {
         { r: 214, g: 93, b: 14 },
         { r: 254, g: 128, b: 25 }
     ];
-
-
     const image = document.getElementById('aurora-img');
-    // const canvas = document.getElementById('aurora-canvas');
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
 
@@ -95,23 +122,7 @@ window.onload = function () {
     ctx.drawImage(image, 0, 0);
     let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-
-
-    for (let y = 0; y < imageData.height; y++) {
-        for (let x = 0; x < imageData.width; x++) {
-            let index = (x + (y * imageData.width)) * 4;
-            let r = imageData.data[index];
-            let g = imageData.data[index + 1];
-            let b = imageData.data[index + 2];
-
-            let closestColor = findClosestPaletteColor(r, g, b, palette);
-
-            imageData.data[index] = closestColor.r;   // Red
-            imageData.data[index + 1] = closestColor.g; // Green
-            imageData.data[index + 2] = closestColor.b; // Blue
-
-        }
-    }
+    quantize(imageData, palette);
 
     image.parentNode.replaceChild(canvas, image);
     ctx.putImageData(imageData, 0, 0);
